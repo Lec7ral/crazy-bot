360b606f0839743241db3dae5c9f3c9441ca5535
Lectral <120367292+Lec7ral@users.noreply.github.com>
2024-10-24 15:00:55 -0400

    Update userSettings.py

--- plugins/userSettings.py
+++ plugins/userSettings.py
@@ -218,7 +218,7 @@ async def user_settings_query(bot, query):
      logging.warning(f"Brinco bien hasta aqui id{chat_id}")
      groups = await get_bot_groups(CLIENT.client(_bot))
      try:
-      groups_in_db = await get_user_channels(user_id)
+      groups_in_db = await db.get_user_channels(user_id)
      except Exception as e:
          logging.error(f"Al buscar en la db {e}")
      logging.warning(f"Bien hasta aqui {groups}")
